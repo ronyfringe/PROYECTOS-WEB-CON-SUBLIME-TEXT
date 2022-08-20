3d12b25ce3ee3443c9bb6d117d19a934674ec0c6
RonyTCdev <rtcardenas26@gmail.com>
2022-08-19 23:31:48 -0500

    aplicando css y js a los botones encabezado

--- Frames/main/cambiando_fondo.js
+++ Frames/main/cambiando_fondo.js
@@ -1,4 +1,24 @@
-    function cambiando_fondo() {
+    function cambiando_fondo_green() {
+        var element = document.body;
+        element.classList.toggle("bgGreen");
+    }
+
+    function cambiando_fondo_blue() {
         var element = document.body;
         element.classList.toggle("bgBlue");
+    }
+
+    function cambiando_fondo_purple() {
+        var element = document.body;
+        element.classList.toggle("bgPurple");
+    }
+
+    function cambiando_fondo_yellow() {
+        var element = document.body;
+        element.classList.toggle("bgYellow");
+    }
+
+     function cambiando_fondo_red() {
+        var element = document.body;
+        element.classList.toggle("bgRed");
     }
